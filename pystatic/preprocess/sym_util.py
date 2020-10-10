9f017ab0a73b5b35aeb62e492ea82c4b44fdca1b
DunderBird <522169030@qq.com>
2020-10-10 21:16:26 +0800

    remove util.py rename ParseException to VisitException

--- pystatic/preprocess/sym_util.py
+++ pystatic/preprocess/sym_util.py
@@ -1,7 +1,7 @@
 import ast
 import logging
-from pystatic.uri import absolute_urilist, Uri, uri2list
-from typing import Optional, TYPE_CHECKING, Any
+from pystatic.uri import absolute_urilist, Uri, uri2list, rel2absuri
+from typing import Optional, TYPE_CHECKING, Union, Dict, Tuple, List
 from pystatic.typesys import (TypeClassTemp, TypeIns, TypeModuleTemp,
                               TypePackageIns, TypeTemp, TypePackageTemp)
 from pystatic.symtable import SymTable, ImportNode
@@ -132,3 +132,33 @@ def search_uri_symtable(symtable: 'SymTable', uri: str) -> Optional[TypeIns]:
         if not cur_ins:
             return None
     return cur_ins
+
+
+def split_import_stmt(node: Union[ast.Import, ast.ImportFrom],
+                      uri: Uri) -> Dict[Uri, List[Tuple[str, str]]]:
+    """Return: imported Moduri mapped to (name1, name2) where name1 is the name in the
+    current module and name2 is the name in the imported module.
+    """
+    res = {}
+    if isinstance(node, ast.Import):
+        for alias in node.names:
+            module_uri = alias.name
+            as_name = alias.asname or module_uri
+            res.setdefault(module_uri, []).append(
+                (as_name, ''))  # empty string means the module itself
+
+    elif isinstance(node, ast.ImportFrom):
+        imp_name = '.' * node.level
+        imp_name += node.module or ''
+        module_uri = rel2absuri(uri, imp_name)
+        imported = []
+        for alias in node.names:
+            attr_name = alias.name
+            as_name = alias.asname or attr_name
+            imported.append((as_name, attr_name))
+        res = {module_uri: imported}
+
+    else:
+        raise TypeError("node doesn't stand for an import statement")
+
+    return res
