0cf020012adae06f8566001d5868e3d23fdb0214
DunderBird <522169030@qq.com>
2020-10-18 21:33:06 +0800

    add opmap

---
+++ pystatic/opmap.py
@@ -0,0 +1,37 @@
+import ast
+
+# MGF: magic function
+MGF_UADD = '__pos__'
+MGF_USUB = '__neg__'
+MGF_INVERT = '__invert__'
+MGF_MULT = '__mul__'
+MGF_ADD = '__add__'
+MGF_SUB = '__sub__'
+
+unaryop_map = {
+    ast.UAdd: MGF_UADD,
+    ast.USub: MGF_USUB,
+    ast.Invert: MGF_INVERT,
+}
+
+binop_map = {
+    ast.Add: MGF_ADD,
+    ast.Sub: MGF_SUB,
+    ast.Mult: MGF_MULT,
+}
+
+binop_char_map = {
+    ast.Add: '+',
+    ast.Sub: '-',
+    ast.Mult: '*',
+    ast.MatMult: '@',
+    ast.Div: '/',
+    ast.Mod: '%',
+    ast.Pow: '**',
+    ast.LShift: '<<',
+    ast.RShift: '>>',
+    ast.BitOr: '|',
+    ast.BitXor: '^',
+    ast.BitAnd: '&',
+    ast.FloorDiv: '//'
+}
