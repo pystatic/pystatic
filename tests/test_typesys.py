f5b03bcac4221284d1e8e908e8bc9944e7247a15
DunderBird <522169030@qq.com>
2020-11-08 11:18:10 +0800

    change __eq__ in typesys to equiv

--- tests/test_typesys.py
+++ tests/test_typesys.py
@@ -17,18 +17,18 @@ def test_eq():
 
     int_ins1 = int_temp.getins(None).value
     int_ins2 = int_temp.getins([]).value
-    assert int_ins1 == int_ins2
+    assert int_ins1.equiv(int_ins2)
 
     float_ins1 = float_temp.getins(None).value
-    assert float_ins1 != int_ins1
+    assert not float_ins1.equiv(int_ins1)
 
     c1 = container_temp.getins([int_ins1, int_ins2]).value
     c2 = container_temp.getins([int_ins2, int_ins1]).value
     c3 = container_temp.getins([float_ins1, int_ins2]).value
-    assert c1 == c2
-    assert c1 != c3
-    assert c2 != c3
+    assert c1.equiv(c2)
+    assert not c1.equiv(c3)
+    assert not c2.equiv(c3)
 
     c4 = container_temp.getins(None).value
     c5 = container_temp.getins([any_ins, any_ins]).value
-    assert c4 == c5
+    assert c4.equiv(c5)
