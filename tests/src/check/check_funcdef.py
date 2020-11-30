271d233effcb60cc1006f4843c7de2a763bdc0f5
hjzs <2113984236@qq.com>
2020-11-30 12:12:41 +0800

    add check_for

--- tests/src/check/check_funcdef.py
+++ tests/src/check/check_funcdef.py
@@ -10,20 +10,12 @@ def f3() -> int:  # E Return value expected
     a: int = 1
 
 
-def f4() -> None:
-    return
-
+# def f4() -> None:
+#     return
+#
 
 def f5() -> None:
     a = 1
 
 
-from typing import Dict, Union, Tuple
-class A:
-    ...
 
-dic: Tuple[int, str, A]
-for s in dic:
-    a: A = s
-    break
-    f=1
